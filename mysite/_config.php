07ad123e95170d03a04e04a0c1e52d5c7b91d8c0
Dustin Quam <quamsta@gmail.com>
2020-11-05 10:15:36 -0600

    updated theme

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,8 +1,8 @@
 <?php
 
-use SilverStripe\Security\PasswordValidator;
-use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\PasswordValidator;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
@@ -11,6 +11,6 @@ $validator->minLength(8);
 $validator->checkHistoricalPasswords(6);
 Member::set_password_validator($validator);
 
-if(Director::isLive()) {
+if (Director::isLive()) {
 	Director::forceSSL();
 }
